976d2dac95b63a66e2777bb67083042ea8ef0c83
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-02-12 23:40:18 +0900

    knexfile設定変更

--- knexfile.js
+++ knexfile.js
@@ -3,32 +3,32 @@ require('dotenv').config();
 
 module.exports = {
   // development : {
+  client: "pg",
+  connection:
+    process.env.DATABASE_URL||
+    `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
+  searchPath: "public",
+  ssl:{ rejectUnauthorized: false } ,
+  migrations: {
+    directory: __dirname + "/migrations"
+  },
+  seeds:{
+    directory: __dirname + "/seeds"
+  },
+  // },
+  
+  production : {
     client: "pg",
     connection:
       process.env.DATABASE_URL||
       `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
     searchPath: "public",
-    ssl:{ rejectUnauthorized: false } ,
+    ssl: { rejectUnauthorized: false } ,
     migrations: {
       directory: __dirname + "/migrations"
     },
     seeds:{
       directory: __dirname + "/seeds"
-    }
-  // },
-  
-  // production : {
-  //   client: "pg",
-  //   connection:
-  //     process.env.DATABASE_URL + "?sslmode=require" ||
-  //     `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
-  //     searchPath: "public",
-  //     ssl: { rejectUnauthorized: false } ,
-  //     migrations: {
-  //       directory: __dirname + "/migrations"
-  //     },
-  //     seeds:{
-  //       directory: __dirname + "/seeds"
-  //     },
-  // }
+    },
+  }
 }
