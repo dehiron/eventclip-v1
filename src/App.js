d43f491a932dbd36b766e9bc83d16330f7aaf1ab
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-02-12 15:52:18 +0900

    シードデータをFEでリスト表示確認

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react"
 
 function App() {
+
+  const [events, setEvents] = useState([]);
+
+  async function fetchData(){
+    await fetch("/api/events")
+    .then((response) => response.json())
+    .then((data) => setEvents(data))
+  }
+
+  useEffect(()=>{
+    fetchData()
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Upcoming Events!</h2>
+        <ul>
+        {events.map((element) => <li key={element.id}>{element.event_name} {element.address}</li>)}
+        </ul>
       </header>
     </div>
   );
