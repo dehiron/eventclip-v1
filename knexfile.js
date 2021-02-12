3244e66aefe38046c8a521bac5654856000e7622
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-02-13 02:01:43 +0900

    knexfile設定変更

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,25 @@
 // Update with your config settings.
 require('dotenv').config();
+const pg = require('pg');
+pg.defaults.ssl = {
+  rejectUnauthorized: false,
+}
 
 module.exports = {
-  // development : {
-  client: "pg",
-  connection:
-    process.env.DATABASE_URL||
-    `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
-  searchPath: "public",
-  ssl:{ rejectUnauthorized: false } ,
-  migrations: {
-    directory: __dirname + "/migrations"
+  development : {
+    client: "pg",
+    connection:
+      process.env.DATABASE_URL||
+      `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
+    searchPath: "public",
+    ssl:{ rejectUnauthorized: false } ,
+    migrations: {
+      directory: __dirname + "/migrations"
+    },
+    seeds:{
+      directory: __dirname + "/seeds"
+    },
   },
-  seeds:{
-    directory: __dirname + "/seeds"
-  },
-  // },
   
   production : {
     client: "pg",
