863fa7427d31ddc5f5c6a785439457f1d195121d
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-02-12 23:33:19 +0900

    knexfile設定変更

--- knexfile.js
+++ knexfile.js
@@ -2,31 +2,33 @@
 require('dotenv').config();
 
 module.exports = {
-  development : {
+  // development : {
     client: "pg",
     connection:
-      process.env.DATABASE_URL ||
+      process.env.DATABASE_URL||
       `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
-      searchPath: "public",
-      migrations: {
-        directory: __dirname + "/migrations"
-      },
-      seeds:{
-        directory: __dirname + "/seeds"
-      }
-  },
-  production : {
-    client: "pg",
-    ssl: { rejectUnauthorized: false } ,
-    connection:
-      process.env.DATABASE_URL + "?sslmode=require" ||
-      `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
-      searchPath: "public",
-      migrations: {
-        directory: __dirname + "/migrations"
-      },
-      seeds:{
-        directory: __dirname + "/seeds"
-      },
-  }
+    searchPath: "public",
+    ssl:{ rejectUnauthorized: false } ,
+    migrations: {
+      directory: __dirname + "/migrations"
+    },
+    seeds:{
+      directory: __dirname + "/seeds"
+    }
+  // },
+  
+  // production : {
+  //   client: "pg",
+  //   connection:
+  //     process.env.DATABASE_URL + "?sslmode=require" ||
+  //     `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
+  //     searchPath: "public",
+  //     ssl: { rejectUnauthorized: false } ,
+  //     migrations: {
+  //       directory: __dirname + "/migrations"
+  //     },
+  //     seeds:{
+  //       directory: __dirname + "/seeds"
+  //     },
+  // }
 }
