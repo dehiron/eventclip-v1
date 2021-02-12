80b1b06ccfd73db72bc4be2b7059a69c8d79a51d
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-02-12 21:48:05 +0900

    knexfile設定変更

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,31 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
-  client: "pg",
-  connection:
-    process.env.DATABASE_URL ||
-    `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
-    searchPath: "public",
-    migrations: {
-      directory: __dirname + "/migrations"
-    },
-    seeds:{
-      directory: __dirname + "/seeds"
-    }
+  development : {
+    client: "pg",
+    connection:
+      process.env.REACT_APP_DATABASE_URL ||
+      `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
+      searchPath: "public",
+      migrations: {
+        directory: __dirname + "/migrations"
+      },
+      seeds:{
+        directory: __dirname + "/seeds"
+      }
+  },
+  production : {
+    client: "pg",
+    connection:
+      process.env.REACT_APP_DATABASE_URL ||
+      `postgres://${process.env.USER}@127.0.0.1:5432/eventclip`,
+      searchPath: "public",
+      migrations: {
+        directory: __dirname + "/migrations"
+      },
+      seeds:{
+        directory: __dirname + "/seeds"
+      }
+  }
 }
