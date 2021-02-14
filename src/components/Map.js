aaef6d31ef53eda17ed829603aac98e556e5f24c
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-02-14 12:29:05 +0900

    マーカークリック時にinfoWindow表示の実装

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useCallback , useRef } from "react";
 // import axios from "axios";
 import "./Styles.css";
-import { GoogleMap, useLoadScript, Marker, 
-    // InfoWindow
-} from "@react-google-maps/api"
-// import { formatRelative } from "date-fns"
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
 //
 //
 //
 import "@reach/combobox/styles.css"
 import mapStyles from "./MapStyles";
+import { formatRelative } from "date-fns";
 require('dotenv').config();
 
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -21,12 +19,12 @@ const mapContainerStyle = {
 const center = {
     lat: 35.681236,
     lng: 139.767125
-}
+};
 const options = {
     styles: mapStyles,
     disableDefaultUI: true,
     zoomControl: true
-}
+};
 
 function Map(props) {
     const { isLoaded, loadError } = useLoadScript({
@@ -34,6 +32,23 @@ function Map(props) {
         libraries
     });
     const [markers, setMarkers] = useState([]);
+    const [selected, setSelected] = useState(null); //選択されたマーカー情報
+    //マーカーを作成する旅にレンダーするのを防ぐためのuseCallback
+    const onMapClick = useCallback((event) => {
+        setMarkers((current) => [
+            ...current, 
+            {
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng(),
+                time: new Date(),
+            }   
+        ]);
+    },[]);
+    //選択したマーカーのステート管理（？）
+    const mapRef = useRef();
+    const onMapLoad = useCallback((map) => {
+        mapRef.current = map;
+    },[])
 
     if (loadError) return "Error loading maps";
     if (!isLoaded) return "Loading Maps";
@@ -51,18 +66,10 @@ function Map(props) {
                 zoom = {10}
                 center = {center}
                 options = {options}
-                onClick = {(event) => {
-                    setMarkers((current) => [
-                        ...current, 
-                        {
-                            lat: event.latLng.lat(),
-                            lng: event.latLng.lng(),
-                            time: new Date()
-                        }   
-                    ]);
-                }}
+                onClick = {onMapClick}
+                onLoad = {onMapLoad}
             >
-                 {/* GoogleMapタグの中身 */}
+                 {/* GoogleMapタグの中身=マーカーの見た目について */}
                  {markers.map((marker) => (
                     <Marker
                         key = {marker.time.toISOString()}
@@ -73,10 +80,25 @@ function Map(props) {
                             anchor: new window.google.maps.Point(15,15),
                             scaledSize: new window.google.maps.Size(30,30)
                         }}
+                        onClick = {() => {
+                            setSelected(marker)
+                        }}
 
 
                     />
                 ))}
+
+                {selected ? (
+                    <InfoWindow 
+                        position = {{ lat: selected.lat, lng: selected.lng }}
+                        onCloseClick = {() => { setSelected(null) }}
+                    >
+                        <div>
+                            <h2>Bear Spotted!</h2>
+                            <p>Spotted {formatRelative(selected.time, new Date())}</p>
+                        </div>
+                    </InfoWindow>
+                ) : null}
             </GoogleMap>
         </div>
 
