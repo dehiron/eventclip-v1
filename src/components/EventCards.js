7fa68a2c41798e2a94d479bff753b6afea642efa
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-02-25 16:40:45 +0900

    イベントリストカードのスタイル微修正

--- src/components/EventCards.js
+++ src/components/EventCards.js
@@ -1,5 +1,5 @@
 //Component
-import { Card, Container, Button, } from 'react-bootstrap';
+import { Card, Container, Button, Row, Col} from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 import Image from 'react-image-resizer';
 
@@ -16,19 +16,23 @@ function EventCards(props){
             {/* (event) => の後に {} 要らない */}
             {props.events.map((event) => 
                 <Card className="listed-event" key = {event.event_name}>
+                <Row>
+                <Col>
                 <Image src={event.img1} alt=""　width={300} height={200}/>
+                </Col>
+                
                 <Card.Body>
                     <Card.Title>
-                    {event.event_name}
+                    <h3> {event.event_name} </h3>
                     </Card.Title>
                     <Card.Text>
                     {event.description}
                     </Card.Text>
                     <Card.Text>
-                    開始: {event.start_date}  {event.start_time}
+                    開催期間: {event.start_date} ~ {event.end_date}
                     </Card.Text>
                     <Card.Text>
-                    終了: {event.end_date}  {event.end_time}
+                    開催時間: {event.start_time} ~ {event.end_time}
                     </Card.Text>
                     <Button 
                                     variant="primary" 
@@ -39,6 +43,8 @@ function EventCards(props){
                                     詳細を見る
                                 </Button>
                 </Card.Body>
+                
+                </Row>
                 </Card>
             )}
         
