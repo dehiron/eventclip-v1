bca653405e07d717fc50270aa5e08444dafd62f2
Hidehiro Aya <hi.dehiro424@gmail.com>
2021-03-03 18:44:33 +0900

    イベントシードデータ追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ function Header (props){
     //   };
 
     // const generator = async () => {
-    //     for (let i = 600; i < 800; i++){
+    //     for (let i = 800; i < 1000; i++){
 
     //         if (i % 200 === 0　&& i !== 0){
     //             console.log(results);
@@ -26,7 +26,7 @@ function Header (props){
     //         await wait(1)
     //         const geoCodeInfo = await getGeocode({address:address[i]});
     //         const {lat,lng} = await getLatLng(geoCodeInfo[0]);
-    //         const result = {id:1010+1+i, event_name:`event${1010+1+i}`, start_date:'2021-01-20', end_date:'2031-06-20', date_detail:"開催期間についての補足情報", category:'スポット', start_time:'11:00', end_time:'17:00', time_detail:"開催時間についての補足情報", state:"関東", prefecture:'東京都', city:'渋谷区', address:address[i], latitude:lat, longitude:lng, facility_name:`event${1010+1+i}施設`, tel:'0300000000', description:'サンプル！', description_detail:"さらに詳しいイベントについての詳細説明",　park_spots:'有/30台',　park_price:'平日8:00~22:00 300円/10分 平日22:00~8:00 100円/60分 土日8:00~22:00 500円/10分 土日22:00~8:00 100円/60分', price_detail:"大人700円　子供300円", credit_card_info:"可", owner_id:'hide_owner', tag:["家族と","デートに","お一人様","癒されたい","ワクワクする"], link_to_hp:'XXXXXXXX.com', img1:'https://picsum.photos/600/400'}
+    //         const result = {id:3010+1+i, event_name:`event${3010+1+i}`, start_date:'2021-01-20', end_date:'2031-06-20', date_detail:"開催期間についての補足情報", category:'スポット', start_time:'11:00', end_time:'17:00', time_detail:"開催時間についての補足情報", state:"関東", prefecture:'東京都', city:'渋谷区', address:address[i], latitude:lat, longitude:lng, facility_name:`event${3010+1+i}施設`, tel:'0300000000', description:'サンプル！', description_detail:"さらに詳しいイベントについての詳細説明",　park_spots:'有/30台',　park_price:'平日8:00~22:00 300円/10分 平日22:00~8:00 100円/60分 土日8:00~22:00 500円/10分 土日22:00~8:00 100円/60分', price_detail:"大人700円　子供300円", credit_card_info:"可", owner_id:1, tag:["家族と","デートに","お一人様","癒されたい","ワクワクする"], link_to_hp:'XXXXXXXX.com', img1:'https://picsum.photos/600/400'}
             
     //         results.push(result);
     //     }
@@ -62,11 +62,11 @@ function Header (props){
                         <Nav.Link onClick={ handleClickToOwnerPage }>イベント登録</Nav.Link>
                     </Nav.Item>
                 </Nav>
-                <Nav>
-                    {/* <Nav.Item>
+                {/* <Nav>
+                    <Nav.Item>
                         <Nav.Link onClick={ generator }>Seed用アドレス取得</Nav.Link>
-                    </Nav.Item> */}
-                </Nav>
+                    </Nav.Item>
+                </Nav> */}
                 <Nav>
                     <Nav.Item>
                         <Nav.Link onClick={ null }>ログイン/新規登録</Nav.Link>
